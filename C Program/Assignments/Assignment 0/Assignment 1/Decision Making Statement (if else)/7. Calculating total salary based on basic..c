69f54d220aa66c72f9a5d005736656c4e62b0c42
Divya Chipade <chipadedivya09@gmail.com>
2026-07-21 20:57:38 +0530

    Update 7. Calculating total salary based on basic..c

--- C Program/Assignments/Assignment 0/Assignment 1/Decision Making Statement (if else)/7. Calculating total salary based on basic..c
+++ C Program/Assignments/Assignment 0/Assignment 1/Decision Making Statement (if else)/7. Calculating total salary based on basic..c
@@ -2,29 +2,26 @@
 
 int main()
 {
-	int basic, da, ta, hra, total;
-	
-	printf("Enter Basic Salary: ");
-	scanf("%d",&basic);
-	
-	if(basic <= 5000)
-	{
-	da = (basic *10)/100;
-    ta=(basic * 20)/100;
-	hra = (basic * 25)/100;
-	total =(basic + da + ta + hra);
-	
-	printf("Total Salary = %d", total);
-	}
-	else
-	{
-	da = (basic *15)/100;
-    ta=(basic * 25)/100;
-	hra = (basic * 30)/100;
-	total =(basic + da + ta + hra);
-	
-	printf("Total Salary = %d", total);
-		
-	}
-	
+    int basic = 4000, da, ta, hra, total;
+
+    if(basic <= 5000)
+    {
+        da = (basic * 10) / 100;
+        ta = (basic * 20) / 100;
+        hra = (basic * 25) / 100;
+        total = basic + da + ta + hra;
+
+        printf("Total Salary = %d", total);
+    }
+    else
+    {
+        da = (basic * 15) / 100;
+        ta = (basic * 25) / 100;
+        hra = (basic * 30) / 100;
+        total = basic + da + ta + hra;
+
+        printf("Total Salary = %d", total);
+    }
+
+    return 0;
 }
