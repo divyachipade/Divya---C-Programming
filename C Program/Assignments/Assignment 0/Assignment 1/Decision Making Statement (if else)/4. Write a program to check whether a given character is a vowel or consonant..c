75c9349195d7439783ac44a87c38ab12cdead80f
Divya Chipade <chipadedivya09@gmail.com>
2026-07-21 20:49:27 +0530

    Update 4. Write a program to check whether a given character is a vowel or consonant..c

--- C Program/Assignments/Assignment 0/Assignment 1/Decision Making Statement (if else)/4. Write a program to check whether a given character is a vowel or consonant..c
+++ C Program/Assignments/Assignment 0/Assignment 1/Decision Making Statement (if else)/4. Write a program to check whether a given character is a vowel or consonant..c
@@ -2,19 +2,16 @@
 
 int main()
 {
-	char ch;
-	
-	printf("Enter an alphabet: ");
-	scanf("%c",&ch);
-	
-	if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'||
-	   ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-	{
-		printf("Vowel");
-	}
-	else
-	{
-		printf("Consonant");
-	}
-	
+    char ch = 'A';
+
+    if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+       ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+    {
+        printf("Vowel");
+    }
+    else
+    {
+        printf("Consonant");
+    }
+
 }
